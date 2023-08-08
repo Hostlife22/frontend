10bac2c75adac074acb6340a552698b8b6e3c22b
hostlife22 <sima.serafim2021@mail.ru>
2023-08-08 14:53:14 +0300

    fix

--- src/js/DocumentList.tsx
+++ src/js/DocumentList.tsx
@@ -3,6 +3,5 @@ import { useTranslation } from 'react-i18next';
 
 export const DocumentList = (): ReactElement => {
   const { t } = useTranslation(['common']);
-	
   return <div style={{ display: 'flex', flexDirection: 'column' }}>{t('common:dark_mode')}</div>;
 };
